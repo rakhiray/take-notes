ad4be8c65471e02bec6bc3371763f7054237af19
syncwelltest2 <rakhiiray14@gmail.com>
2023-05-24 19:05:31 +0530

    css updated

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -10,8 +10,9 @@ const Cards = (props) => {
   return (
     <>
       <div className="cards">
-        <h1>{props.title}</h1>
-        <p>{props.text}</p>
+        <h1 id="card-title">{props.title}</h1>
+
+        <p id="card-content">{props.text}</p>
         <Button className="btn" onClick={delCard}>
           <DeleteOutlineIcon id="del-icon" />
         </Button>
